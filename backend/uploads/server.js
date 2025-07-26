78d8196f82f77d789d65f8d5cf8f65a89e7131fa
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 21:24:17 +0800

    server.js

--- backend/uploads/server.js
+++ backend/uploads/server.js
@@ -1,1 +1,10 @@
+// File: backend/server.js 
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => { console.log(Server running on http://localhost:${PORT}); });
+
+
 
