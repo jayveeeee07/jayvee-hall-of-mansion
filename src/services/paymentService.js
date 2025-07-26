adf2850b5f2bb1e45f4dcfe3aaa38a35ce021e9e
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 20:56:23 +0800

    paymentService.js

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -1,1 +1,7 @@
+// File: src/services/paymentService.js 
+
+import api from "./api";
+
+export const submitPayment = (paymentData) => api.post("/payments", paymentData); 
+export const getPayments = () => api.get("/payments");
 
