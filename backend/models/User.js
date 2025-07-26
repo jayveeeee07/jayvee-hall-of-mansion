d5e690bf3380e1197db23d2d0f27a683f5de3f3d
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 21:42:16 +0800

    User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,8 @@
+// File: backend/models/User.js 
 
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({ full_name: { type: String, required: true }, birthdate: { type: Date, required: true }, status: { type: String, required: true }, sex: { type: String, enum: ['Male', 'Female', 'Other'], required: true }, address: { type: String, required: true }, contact_number: { type: String, required: true }, email: { type: String, required: true, unique: true }, password: { type: String, required: true }, role: { type: String, enum: ['customer', 'admin', 'staff'], default: 'customer' } }, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
+
+                                                                                                                                                                                                       
