09931e68ac0f29a7ac2268b41e64fd65d6f1338f
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 20:35:49 +0800

    ProtectedRoutes.jsx

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,1 +1,15 @@
+// File: src/components/ProtectedRoutes.jsx 
+
+import React from "react"; 
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoutes = () => { const isAuthenticated = !!localStorage.getItem("user");
+
+return isAuthenticated ? <Outlet /> : <Navigate to="/login" />; };
+
+export default ProtectedRoutes;
+
+
+
+
 
