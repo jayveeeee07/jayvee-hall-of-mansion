510c500d9d0124fc2a90125094458e70f892bfce
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 21:40:54 +0800

    Payment.js

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,10 +1,3 @@
-// File: backend/models/Room.js 
-
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({ room_number: { type: String, required: true }, price: { type: Number, required: true }, image_url: { type: String }, description: { type: String } }, { timestamps: true });
-
-module.exports = mongoose.model('Room', roomSchema);
 
 
 
