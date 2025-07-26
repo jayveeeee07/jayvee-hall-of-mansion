7a107797cb0c2945d805170380081f0a8deda44f
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 20:55:13 +0800

    bookingService.js

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,1 +1,8 @@
+// File: src/services/bookingService.js
+
+import api from "./api";
+
+export const bookRoom = (bookingData) => api.post("/bookings", bookingData); 
+export const getBookings = () => api.get("/bookings"); 
+export const approveBooking = (id) => api.put(/bookings/${id}/approve); export const denyBooking = (id) => api.put(/bookings/${id}/deny);
 
