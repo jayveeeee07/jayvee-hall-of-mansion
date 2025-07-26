12d065297b7f32ae25c689e3a451381659153281
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 21:55:40 +0800

    Payment.js

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,4 +1,19 @@
+// File: backend/models/Payment.js
+const mongoose = require('mongoose');
 
+const paymentSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  booking_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking' },
+  amount: { type: Number, required: true },
+  method: { type: String, enum: ['Gcash', 'PayPal', 'Seabank'], required: true },
+  reference_number: { type: String, required: true },
+  proof: { type: String, required: true },
+  status: { type: String, enum: ['pending', 'confirmed', 'failed'], default: 'pending' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Payment', paymentSchema);
+
+           
 
 
 
