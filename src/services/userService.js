f58d3cb35c39fe9f594884650bfdd7caf87a1145
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 20:57:18 +0800

    userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,12 @@
+// File: src/services/userService.js 
 
+import api from "./api";
+
+export const getUserInfo = (userId) => api.get(/users/${userId}); 
+export const updateUser = (userId, data) => api.put(/users/${userId}, data); 
+export const getAllUsers = () => api.get("/users");
+
+
+  
+
+  
