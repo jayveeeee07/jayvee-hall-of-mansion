23df0588246e46c226172aa8027120119a67fdcc
Jayvee Caringal <144330185+jayveeeee07@users.noreply.github.com>
2025-07-26 20:53:57 +0800

    authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,8 @@
+// File: src/services/authService.js 
+
+import api from "./api";
+
+export const login = (credentials) => api.post("/auth/login", credentials); 
+export const register = (userData) => api.post("/auth/register", userData);
+export const adminLogin = (credentials) => api.post("/auth/admin-login", credentials);
 
